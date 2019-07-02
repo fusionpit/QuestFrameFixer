5a2b36348f222811416bf7527f123a984eefce8e
fusionpit <fusionpit@gmail.com>
2019-07-01 21:25:04 -0600

    format qff

--- QuestFrameFixer.lua
+++ QuestFrameFixer.lua
@@ -1,13 +1,16 @@
-QuestFrameGreetingPanel:HookScript("OnShow", function()
-    for i = 1, MAX_NUM_QUESTS do
-        local titleLine = _G["QuestTitleButton"..i];
-        if (titleLine:IsVisible()) then
-            local bulletPointTexture = _G[titleLine:GetName().."QuestIcon"];
-            if (titleLine.isActive == 1) then
-                bulletPointTexture:SetTexture("Interface\\GossipFrame\\ActiveQuestIcon");
-            else
-                bulletPointTexture:SetTexture("Interface\\GossipFrame\\AvailableQuestIcon");
+QuestFrameGreetingPanel:HookScript(
+    "OnShow",
+    function()
+        for i = 1, MAX_NUM_QUESTS do
+            local titleLine = _G["QuestTitleButton" .. i]
+            if (titleLine:IsVisible()) then
+                local bulletPointTexture = _G[titleLine:GetName() .. "QuestIcon"]
+                if (titleLine.isActive == 1) then
+                    bulletPointTexture:SetTexture("Interface\\GossipFrame\\ActiveQuestIcon")
+                else
+                    bulletPointTexture:SetTexture("Interface\\GossipFrame\\AvailableQuestIcon")
+                end
             end
         end
     end
-end)
+)
