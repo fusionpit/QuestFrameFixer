89b7199aaff507cd5d1b6f73ff3f3e39efcff911
fusionpit <fusionpit@gmail.com>
2022-10-08 13:25:38 -0600

    fix daily and repeatable quest icons

--- QuestFrameFixer.lua
+++ QuestFrameFixer.lua
@@ -1,30 +1,36 @@
-local _G = _G
-local tinsert = tinsert
-local GetFileIDFromPath = GetFileIDFromPath
-local MAX_NUM_QUESTS = MAX_NUM_QUESTS
+local DAILY_AVAILABLE_QUEST_ICON_FILEID = GetFileIDFromPath("Interface\\GossipFrame\\DailyQuestIcon")
+local REPEAT_QUEST_ICON_FILEID = GetFileIDFromPath("Interface\\GossipFrame\\DailyActiveQuestIcon")
 
-local ACTIVE_QUEST_ICON_FILEID = GetFileIDFromPath("Interface\\GossipFrame\\ActiveQuestIcon")
-local AVAILABLE_QUEST_ICON_FILEID = GetFileIDFromPath("Interface\\GossipFrame\\AvailableQuestIcon")
-
-local titleLines = {}
-local questIconTextures = {}
-for i = 1, MAX_NUM_QUESTS do
-    local titleLine = _G["QuestTitleButton" .. i]
-    tinsert(titleLines, titleLine)
-    tinsert(questIconTextures, _G[titleLine:GetName() .. "QuestIcon"])
+local function getLineAndIconMaps(maxLines, titleIdent, iconIdent)
+    local lines = {}
+    local icons = {}
+    for i = 1, maxLines do
+        local titleLine = _G[titleIdent .. i]
+        tinsert(lines, titleLine)
+        tinsert(icons, _G[titleLine:GetName() .. iconIdent])
+    end
+    return lines, icons
 end
-QuestFrameGreetingPanel:HookScript(
-    "OnShow",
-    function()
-        for i, titleLine in ipairs(titleLines) do
-            if (titleLine:IsVisible()) then
-                local bulletPointTexture = questIconTextures[i]
-                if (titleLine.isActive == 1) then
-                    bulletPointTexture:SetTexture(ACTIVE_QUEST_ICON_FILEID)
-                else
-                    bulletPointTexture:SetTexture(AVAILABLE_QUEST_ICON_FILEID)
-                end
+
+local function markDailies(lines, textures, ...)
+    local lineIndex = 0
+    for i=1, select("#", ...), 7 do
+        lineIndex = lineIndex + 1
+        local activeQuestLine = lines[lineIndex]
+        if activeQuestLine:IsVisible() and activeQuestLine.type == 'Available' then
+            local _, _, _, frequency, isRepeatable = select(i, ...)
+            if isRepeatable then
+                local texture = textures[lineIndex]
+                texture:SetTexture(REPEAT_QUEST_ICON_FILEID)
+            elseif (frequency == LE_QUEST_FREQUENCY_DAILY or frequency == LE_QUEST_FREQUENCY_WEEKLY) then
+                local texture = textures[lineIndex]
+                texture:SetTexture(DAILY_AVAILABLE_QUEST_ICON_FILEID)
             end
         end
     end
-)
+end
+
+local gossipTitleLines, gossipIconTextures = getLineAndIconMaps(NUMGOSSIPBUTTONS, "GossipTitleButton", "GossipIcon")
+hooksecurefunc("GossipFrameUpdate", function()
+    markDailies(gossipTitleLines, gossipIconTextures, GetGossipAvailableQuests())
+end)
