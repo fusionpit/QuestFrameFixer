e04b224f310a69c855f786a5ce5b3d6736cfed68
fusionpit <fusionpit@gmail.com>
2025-12-14 20:24:32 -0700

    fix tbc quests showing up with the 'daily' icon
    (cherry picked from commit 516ed9a0bdbd3084d945ca3b165e65a34273cc0b)

--- QuestFrameFixer.lua
+++ QuestFrameFixer.lua
@@ -3,8 +3,15 @@ local tinsert = tinsert
 local GetFileIDFromPath = GetFileIDFromPath
 local MAX_NUM_QUESTS = MAX_NUM_QUESTS
 
-local ACTIVE_QUEST_ICON_FILEID = GetFileIDFromPath("Interface\\GossipFrame\\ActiveQuestIcon")
-local AVAILABLE_QUEST_ICON_FILEID = GetFileIDFromPath("Interface\\GossipFrame\\AvailableQuestIcon")
+local ICONS = {
+    activeQuest = GetFileIDFromPath("Interface\\GossipFrame\\ActiveQuestIcon"),
+    availableQuest = GetFileIDFromPath("Interface\\GossipFrame\\AvailableQuestIcon")
+}
+if ClassicExpansionAtLeast(LE_EXPANSION_BURNING_CRUSADE) then
+    ICONS.dailyQuest = GetFileIDFromPath("Interface\\GossipFrame\\DailyQuestIcon")
+    ICONS.repeatableQuest = GetFileIDFromPath("Interface\\GossipFrame\\DailyActiveQuestIcon")
+    ICONS.legendaryQuest = GetFileIDFromPath("Interface\\GossipFrame\\AvailableLegendaryQuestIcon")
+end
 
 local titleLines = {}
 local questIconTextures = {}
@@ -13,17 +20,36 @@ for i = 1, MAX_NUM_QUESTS do
     tinsert(titleLines, titleLine)
     tinsert(questIconTextures, _G[titleLine:GetName() .. "QuestIcon"])
 end
+-- In Classic Era, the QuestFrame uses little yellow dots for both active and available quests
+local function setClassicTexture(titleLine, iconTexture)
+    if titleLine.isActive == 1 then
+        iconTexture:SetTexture(ICONS.activeQuest)
+    else
+        iconTexture:SetTexture(ICONS.availableQuest)
+    end
+end
+-- In TBC Classic (the second iteration), the QuestFrame incorrectly marks all available quests as a daily quest (except for legendary)
+local function setTbcTexture(titleLine, iconTexture)
+    if titleLine.isActive == 1 then return end
+    local _, frequency, isRepeatable, isLegendary = GetAvailableQuestInfo(titleLine:GetID())
+    if isLegendary then
+        iconTexture:SetTexture(ICONS.legendaryQuest)
+    elseif frequency > 1 then
+        iconTexture:SetTexture(ICONS.dailyQuest)
+    elseif isRepeatable then
+        iconTexture:SetTexture(ICONS.repeatableQuest)
+    else
+        iconTexture:SetTexture(ICONS.availableQuest)
+    end
+end
+local setTexture = ClassicExpansionAtLeast(LE_EXPANSION_BURNING_CRUSADE) and setTbcTexture or setClassicTexture
 QuestFrameGreetingPanel:HookScript(
     "OnShow",
     function()
         for i, titleLine in ipairs(titleLines) do
-            if (titleLine:IsVisible()) then
-                local bulletPointTexture = questIconTextures[i]
-                if (titleLine.isActive == 1) then
-                    bulletPointTexture:SetTexture(ACTIVE_QUEST_ICON_FILEID)
-                else
-                    bulletPointTexture:SetTexture(AVAILABLE_QUEST_ICON_FILEID)
-                end
+            if titleLine:IsVisible() then
+                local iconTexture = questIconTextures[i]
+                setTexture(titleLine, iconTexture)
             end
         end
     end
